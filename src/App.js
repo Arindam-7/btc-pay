21943d77f9e5090b39b5dc3c59fa85f27de9b361
Arindam Saha <msaha6368@gmail.com>
2022-01-30 22:23:32 +0530

    ready to push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Link, Route, Switch } from "react-router-dom";
+import { CardGrid } from "./components/CardGrid";
+import { Layout, Menu } from "antd";
+const { Header, Content, Footer } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const NotFound = () => <h1>Not Found</h1>;
+const Paid = () => <h1>THANKS FOR YOUR PAYMENT!</h1>;
+
+class App extends React.Component {
+  render() {
+    return (
+      <Layout style={{ height: "auto" }}>
+        <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+          <img
+            src={require('./images/download.jpeg')}
+            alt={"shopIcon"}
+            height={"64px"}
+            style={{ float: "left", marginRight: "20px" }}
+          />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            style={{ lineHeight: "64px" }}
+          >
+            <Menu.Item key="1">
+              <Link to="/">Pay in Bitcoin</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content
+          className={"App"}
+          style={{ padding: "0 50px", marginTop: 84, height: "100%" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Switch>
+            <Route path="/" exact component={CardGrid} />
+            <Route path="/paid/" component={Paid} />
+            <Route component={NotFound} />
+          </Switch>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Hostelio ©2022
+        </Footer>
+      </Layout>
+    );
+  }
 }
 
 export default App;
